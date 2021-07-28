5f15600326a072094c1b0e1f7fad3853dadc75b8
Mostafa Md. Reyad Rahman <reyadrahman@users.noreply.github.com>
2021-07-28 21:38:04 +0600

    Add files via upload

--- app/src/main/java/com/mutho/games/MyApplication.java
+++ app/src/main/java/com/mutho/games/MyApplication.java
@@ -1,1 +1,16 @@
+package com.mutho.games;
 
+import android.app.Application;
+
+
+import com.facebook.ads.AudienceNetworkAds;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AudienceNetworkAds.initialize(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+    }
+}
